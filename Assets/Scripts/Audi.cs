5a4e32812e3e3319be7206afa5ef8d5068d4903a
mrobinson-ad <matthew.rob6@gmail.com>
2024-11-29 12:49:35 +0100

    working build

---
+++ Assets/Scripts/Audi.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audi : MonoBehaviour, ICarInteraction 
+{
+    public AudioClip horn;
+    public AudioClip engine;
+    public AudioSource carAudio;
+
+   
+
+    public void StartEngine()
+    {
+        carAudio.PlayOneShot(engine, 0.5f);
+    }
+
+    public void HonkHorn()
+    {
+        carAudio.PlayOneShot(horn, 0.7f);
+    }
+
+    public void ToggleDoor(DoorType doorType)
+    {
+       
+    }
+
+
+}
